ce97e4ace782db0f7a0e6a8f50296ff9f1ffb9ad
Rania-smt27 <101665584+Rania-smt27@users.noreply.github.com>
2022-03-21 17:17:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var unirest =require("unirest");
 var req = unirest("GET", "http://localhost:3004/Jobs?top=10");
+const char = 'M';
 
 req.headers({
     "cache-control": "no-cache",
 });
 
-;
+if (req.startsWith(char){
+    req.satus(200);
+    }
 req.end(function(res){
     if(res.error) throw new Error(res.error)
     console.log(res.body)
